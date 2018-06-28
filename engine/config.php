e907b14b31403d88cd5df94c72761f177c2a6838
Bart11s <40652818+Bart11s@users.noreply.github.com>
2018-06-28 11:56:12 +0500

    init commit

---
+++ engine/config.php
@@ -0,0 +1,19 @@
+<?php
+
+if( !defined("IN_PAGE") )
+{
+	header("location: https://www.google.ru/");
+	die();
+}
+
+define("MYSQL_HOST", "localhost");
+define("MYSQL_USER", "dolbaeb3000");
+define("MYSQL_PASS", "russianrus32");
+define("MYSQL_DB", "id6003_loader");
+
+define("PANEL_LOGIN", "dolbaeb3000");
+define("PANEL_PASS", "russianrus32");
+
+define("BUILD_PATH","build/")
+
+?>
